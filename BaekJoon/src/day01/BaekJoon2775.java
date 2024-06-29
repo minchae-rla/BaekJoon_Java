ded12fa443d9aa4dbf2f5c1fa6578ed43f9a84ef
user <user@DESKTOP-3IGNUFI>
2024-06-29 18:55:40 +0900

    day01

--- BaekJoon/src/day01/BaekJoon2775.java
+++ BaekJoon/src/day01/BaekJoon2775.java
@@ -1,9 +1,9 @@
 package day01;
 
-public class Main {
+public class BaekJoon2775 {
 	public static void main(String[] args) {
+		//문제번호.2775.Hello World!를 출력하시오.
 		System.out.println("Hello World!");
-		System.out.println("hi");
 	}
 
 }
