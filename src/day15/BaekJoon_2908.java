f9807812395d7921b5a16e186c40fad944a07f6b
kimminchae <0720kmc@naver.com>
2024-07-17 22:55:32 +0900

    day15

---
+++ src/day15/BaekJoon_2908.java
@@ -0,0 +1,24 @@
+package day15;
+
+import java.util.Scanner;
+
+public class BaekJoon_2908 {
+	public static void main(String[] args) {
+		//문제번호.2908
+		//상근이의 동생 상수는 수학을 정말 못한다. 상수는 숫자를 읽는데 문제가 있다. 이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다. 
+		//상근이는 세 자리 수 두 개를 칠판에 써주었다. 그 다음에 크기가 큰 수를 말해보라고 했다.
+		//상수는 수를 다른 사람과 다르게 거꾸로 읽는다. 예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다. 따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
+		//두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		num1 = Integer.parseInt(new StringBuilder().append(num1).reverse().toString());
+		num2 = Integer.parseInt(new StringBuilder().append(num2).reverse().toString());
+		
+		System.out.print(num1 > num2 ? num1 : num2);
+	}
+
+}
