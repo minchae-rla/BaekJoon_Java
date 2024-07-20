d3fbe69599ef812596390b8bf611eb5676d24f1c
kimminchae <0720kmc@naver.com>
2024-07-20 17:43:35 +0900

    day16

---
+++ src/day16/BaekJoon_11718.java
@@ -0,0 +1,19 @@
+package day16;
+
+import java.util.Scanner;
+
+public class BaekJoon_11718 {
+	public static void main(String[] args) {
+		// 문제번호.11718
+		// 입력 받은 대로 출력하는 프로그램을 작성하시오.
+
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNextLine()) {
+			String str = sc.nextLine();
+
+			System.out.println(str);
+		}
+		sc.close();
+	}
+}
